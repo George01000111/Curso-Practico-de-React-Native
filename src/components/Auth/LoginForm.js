9d2e8824194b0d4744c80670753ee1ffbf8b3b13
george <george_soft_01@hotmail.com>
2022-01-15 01:59:38 -0500

    quede 15-01-22

--- src/components/Auth/LoginForm.js
+++ src/components/Auth/LoginForm.js
@@ -5,11 +5,40 @@ export default function LoginForm() {
     return (
         <View>
             <Text style={styles.title}>Iniciar sesion</Text>
+            <TextInput
+              placeholder="Nombre de usuario"
+              style={styles.input}
+              autoCapitalize='none'
+            />
+            <TextInput
+              placeholder="Contrasena" 
+              style={styles.input} 
+              autoCapitalize='none'
+              secureTextEntry={true}         
+            />
+
+            <Button title="Entrar" onPress={()=>console.log("entrando...")} />
         </View>
     )
 }
 
 
 const styles=StyleSheet.create({
-    
+    title:{
+        textAlign:"center",
+        fontSize:28,
+        fontWeight:"bold",
+        marginTop:50,
+        marginBottom:15,
+
+    },
+
+    input:{
+     height:40,
+     margin:12,
+     borderWidth:1,
+     padding:10,
+     borderRadius:10,
+    }
+
 })
